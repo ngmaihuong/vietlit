2946db9cca4289d543a4d627ef66c6269bc27991
Sierra <maihuongnguyen1603@gmail.com>
2020-12-18 16:51:33 -0500

    Update userop_cleaning.py

--- userop_cleaning.py
+++ userop_cleaning.py
@@ -3,7 +3,7 @@
 """
 Author:         Sierra Nguyen
 Date created:   12/08/2020
-Date updated:   12/17/2020
+Date updated:   12/18/2020
 Title:          VietLit User Survey Data Cleaning
 """
 
@@ -93,9 +93,6 @@ word_count = word_count.rename(columns={0: 'ct'})
 # filtering by count
 word_count = word_count[word_count.ct > 5]
 
-for i in range(len(word_count)):
-    print(word_count.index[i])
-
 # POS tagging
     
 # =============================================================================
@@ -125,6 +122,7 @@ word_tag = list(word_count.index.values)
 for i in range(len(word_count)):
     word_tag[i] = pos_tag(word_count.index[i])[0]
 
+# finding a list of POS
 key_list = list(word_count.index.values)
 values_list = []
 
@@ -132,111 +130,27 @@ for i in range(len(word_tag)):
     values_list.append(word_tag[i][1])
 
 word_tag_dict = dict(zip(key_list, values_list))
+pos_list = set(list(word_tag_dict.values()))
 
-# =============================================================================
-# # POS tagging
-# from underthesea import pos_tag
-# tagged_master_string = pos_tag(master_string)
-# 
-# # sentiment analysis
-# from underthesea import sentiment
-# for i in range(len(interest)):
-#     interest[i] = sentiment(interest[i])
-# =============================================================================
+# getting list of relevant words   
+word_cloud = []
+for item in word_tag:
+    if item[1] in ['A', 'M', 'N', 'V']:
+        if (len(item[0]) in [2, 3]) and item[1] == 'A':
+            word_cloud.append(item[0])
+        elif (len(item[0]) in [4, 5] and item[1] in ['A', 'M']):
+            word_cloud.append(item[0])
+        elif len(item[0]) > 5:
+            word_cloud.append(item[0])
 
-# =============================================================================
-# # separating the data set into long and short responses
-# long_answer_row = []
-# short_answer_row = []
-# 
-# for i in range(len(crit)):
-#     if len(crit[i]) > 75:
-#         long_answer_row.append(i)
-#     else:
-#         short_answer_row.append(i)
-# 
-# crit_long = crit.iloc[long_answer_row]
-# crit_short = crit.iloc[short_answer_row]
-#         
-# for i in range(len(reas)):
-#     if len(reas[i]) > 75:
-#         long_answer_row.append(i)
-#     else:
-#         short_answer_row.append(i)
-# 
-# reas_long = reas.iloc[long_answer_row]
-# reas_short = reas.iloc[short_answer_row]
-# 
-# # =============================================================================
-# # for i in range(df1.shape[0]):
-# #     for j in range(df1.shape[1]):
-# #         if len(df1.iloc[i, j]) > 100:
-# #             long_answer_row.append(i)
-# #         else:
-# #             short_answer_row.append(i)
-# # =============================================================================
-# 
-# def get_unique(index_list):
-#     index_list = set(index_list)
-#     index_list = list(index_list)
-#     return index_list
-#     
-# long_answer_row = get_unique(long_answer_row)
-# short_answer_row = get_unique(short_answer_row)
-# 
-# df2 = df1.iloc[long_answer_row]
-# df3 = df1.iloc[short_answer_row]
-# 
-# # exporting tables
-# df2.to_excel('long_resp.xlsx', index = True)
-# df3.to_excel('short_resp.xlsx', index = True)
-# 
-# master_string = ''
-# 
-# # processing short responses
-# 
-# # combining all cells into a paragraph
-# for i in range(df3.shape[1]):
-#     for j in range(df3.shape[0]):
-#         master_string += ' ' + df3.iloc[j, i]
-# 
-# # stripping punctuation
-# master_string = master_string.translate(str.maketrans('', '', string.punctuation))
-# 
-# # splitting master string into a list of words       
-# master_list = master_string.split()
-# master_list = [x.lower() for x in master_list]
-# 
-# # getting list of words and counts
-# word_count = dict(Counter(master_list))
-# 
-# word_count = pd.DataFrame.from_dict(word_count, orient='index')
-# word_count = word_count.rename(columns={0: 'ct'})
-# 
-# # filtering by count
-# word_count = word_count[word_count.ct > 4]
-# 
-# # filtering by word
-# drop_list = [' người ', ' đọc ', ' truyện ', ' và ', ' cần ', ' sẽ ', ' mình ', ' đủ ', ' quá ', 
-#              ' là ', ' được ', ' ạ ', ' vì ', ' chưa ', ' nên ', ' sách ', ' với ', ' việt ', ' văn ', 
-#              ' để ', ' cũng ', ' như ', ' k ', ' từ ', ' nơi ', ' cho ', ' hơn ', ' muốn ', ' mọi ', ' rất ', 
-#              ' thì ', ' ở ', ' khá ', ' nó ', ' vậy ', ' mong ']
-# drop_list_no_space = [x.strip(' ') for x in drop_list]
-# word_count = word_count.drop(drop_list_no_space)#.reset_index().rename(columns={'index': 'word'})
-# 
-# # stripping punctuations and words with no meanin
-# for i in range(df2.shape[1]):
-#     for j in range(df2.shape[0]):
-#        df2.iloc[j, i] = df2.iloc[j, i].translate(str.maketrans('', '', string.punctuation))
-#        for k in range(len(drop_list)):
-#            if drop_list[k] in df2.iloc[j, i]:
-#                df2.iloc[j, i] = df2.iloc[j, i].replace(drop_list[k], ' ')
-# =============================================================================
-       
-# =============================================================================
-# # exporting list for Tableau
-# from pandas import DataFrame
-# wc_df = DataFrame(master_list)
-# wc_df.to_excel('short_resp_wc.xlsx', index=True)
-# =============================================================================
+# retaining count of relevant words
+word_count = word_count.reset_index()
+word_cloud = pd.DataFrame(word_cloud, columns=['word'])
+word_cloud = pd.merge(word_cloud, word_count, how='left', right_on='index', left_on='word').drop(columns=['index']).dropna()
+
+# exporting
+interest.to_excel('vietlist_user_interest.xlsx', index = True)
+word_cloud.to_excel('vietlit_word_cloud.xlsx', index = True)
+
+# end
 
